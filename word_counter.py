42987378d457915e29727207b294272198c6b771
himam27 <144701588+himam27@users.noreply.github.com>
2023-09-12 19:55:16 +0000

    name change

---
+++ word_counter.py
@@ -0,0 +1,8 @@
+print ('Ce programe compte le nombre de charactères dans le mot ou même phrase ')
+mot = input('Écrivez ce que vous voulez:')
+
+def count_word ():
+    count= len (mot)
+    return count
+
+print ('voici la quantitée de lettres utilisées : %d'%count_word())
