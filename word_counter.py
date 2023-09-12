7646b716cc4ca99b02ea5ea5b710ad2705060e53
mat <144701588+himam27@users.noreply.github.com>
2023-09-12 16:12:26 -0400

    word_counter.py

--- word_counter.py
+++ word_counter.py
@@ -2,7 +2,7 @@ print ('Ce programe compte le nombre de charactères dans le mot ou même phrase
 mot = input('Écrivez ce que vous voulez:')
 
 def count_word ():
-    count= len (mot)
+    count= len (phrase.split())
     return count
 
 print ('voici la quantitée de lettres utilisées : %d'%count_word())
